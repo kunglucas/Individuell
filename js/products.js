a8c87c4f5c774e544992e7b387f75c3a5c34369a
Lucas Larsson <lucas.larsson93@hotmail.com>
2022-11-25 09:05:14 +0100

    New background color and sidenav color

--- js/products.js
+++ js/products.js
@@ -1,23 +1,58 @@
-   // Jquery to hover the jumbotron and display Motod.
+   // Jquery to hover the panel/jumbotron and display slogan.
    $(".panel").hover(function () {
     $(this).append($("<span class='hid'> Just Buy It! </span>"));
 },function () {
     $(this).find('span').slideUp('fast');
 });
 
-// Show when button is clicked by user.
+// Display more btn.
 $(document).ready(function () {
 
+$("#p10").hide();    
+$("#p9").hide();    
+$("#p8").hide();
+$("#p7").hide();
+$("#p6").hide();        
 $("#p5").hide();
 $("#p4").hide();
 $("#p3").hide();
 $("#p2").hide();
 $("#p1").hide();
+$("#showNumbTen").show();
+$("#showNumbNine").show();
+$("#showNumbEight").show();
+$("#showNumbSeven").show();
+$("#showNumbSix").show();
 $("#showNumbFive").show();
 $("#showNumbFour").show();
 $("showNumbThree").show();
 $("showNumbTwo").show();
 $("showNumbOne").show();
+
+$("#showNumbTen").click(function(){
+    $("#p10").toggle();
+    $("#showNumbTen").html() === 'Visa mer' ? $("#showNumbTen").html('Visa mindre') : $("#showNumbTen").html('Visa mer');
+});
+
+$("#showNumbNine").click(function(){
+    $("#p9").toggle();
+    $("#showNumbNine").html() === 'Visa mer' ? $("#showNumbNine").html('Visa mindre') : $("#showNumbNine").html('Visa mer');
+});
+
+$("#showNumbEight").click(function(){
+    $("#p8").toggle();
+    $("#showNumbEight").html() === 'Visa mer' ? $("#showNumbEight").html('Visa mindre') : $("#showNumbEight").html('Visa mer');
+});
+
+$("#showNumbSeven").click(function(){
+    $("#p7").toggle();
+    $("#showNumbSeven").html() === 'Visa mer' ? $("#showNumbSeven").html('Visa mindre') : $("#showNumbSeven").html('Visa mer');
+});
+
+$("#showNumbSix").click(function(){
+    $("#p6").toggle();
+    $("#showNumbSix").html() === 'Visa mer' ? $("#showNumbSix").html('Visa mindre') : $("#showNumbSix").html('Visa mer');
+});
 
 $("#showNumbFive").click(function(){
      $("#p5").toggle();
